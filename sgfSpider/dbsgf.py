45ca79ff4e6ab3132f2cf4b8bffc3d74dc9d9628
david hodges <david.hodges@lonelyplanet.com>
2015-08-22 00:04:23 +0100

    restore all DB items to the same source file
    this is a bit lazy, but it's the easiest way for DBsgf to have access to the same Base object used by all table classes. This same Base instance is needed by DBsgf when creating the schema.

--- sgfSpider/dbsgf.py
+++ sgfSpider/dbsgf.py
@@ -1,35 +1,71 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy     import MetaData
-from sqlalchemy     import create_engine
-from sqlalchemy.sql import table
-from sqlalchemy.orm import sessionmaker
-
-from sgfSpider.items   import IgokisenGameItem
-from sgfSpider.items   import IgokisenNewsItem
-from sgfSpider.dbitems import DBGameItem
-from sgfSpider.dbitems import DBNewsItem
-
 import os
 import pdb
 import contextlib
 import logging
 
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy       import Column, Integer, String, Date, Sequence
+from sqlalchemy       import create_engine
+from sqlalchemy.sql   import table
+from sqlalchemy.orm   import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
+from sgfSpider.items   import IgokisenGameItem
+from sgfSpider.items   import IgokisenNewsItem
+
+
+
 Base = declarative_base()
+
+class DBNewsItem(Base):
+  __tablename__ = 'tournament_news_items'
+  id     = Column(Integer, Sequence('tournament_news_item_id_seq'), primary_key=True)
+  date   = Column(Date)
+  site   = Column(String)
+  game   = Column(String)
+  nation = Column(String)
+  link   = Column(String)
+
+  def __repr__(self):
+    return "<TournamentNewsItem(date='%s', nation='%s', game='%s')>" % (
+      self.date, self.nation, self.game)
+
+
+class DBGameItem(Base):
+  __tablename__ = 'tournament_games'
+  id          = Column(Integer, Sequence('tournament_games_id_seq'), primary_key=True)
+  date        = Column(String)
+  link        = Column(String)
+  sgf         = Column(String)
+  event       = Column(String)
+  tournament  = Column(String)
+  playerBlack = Column(String)
+  playerWhite = Column(String)
+  result      = Column(String)
+
+  def __repr__(self):
+    return "<TournamentGameItem(date='%s', tournament='%s', event='%s', playerBlack='%s', playerWhite='%s', result='%s')>" % (
+      self.date, self.tournament, self.event, self.playerBlack, self.playerWhite, self.result)
+
+
 
 class DBsgf():
   def __init__(self):
     self._setupEngine()
     self.session = sessionmaker(bind=self.engine)()
+    self._setupDB()
 
   def _setupEngine(self):
     if not 'DB_URL' in os.environ.keys():
       raise Exception("os.environ['DB_URL'] undefined")
-    self.engine = create_engine(
-        os.environ['DB_URL'],
-        isolation_level="READ UNCOMMITTED"
-    )
+    self.engine = create_engine(os.environ['DB_URL'])
+
+  def _setupDB(self):
+    if not database_exists(self.engine.url):
+      create_database(self.engine.url)
     Base.metadata.create_all(self.engine)
 
   def _deleteAllTables(self):
